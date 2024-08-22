9d74cec44ff751d164e5383ae2b659d368eb76d4
Jefferson Santana <jeffersonsantana1914@outlook.com>
2024-08-22 13:48:39 -0300

    disappear image

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function createDecodedText() {
+  const heading = document.querySelector('h1');
+  heading.innerHTML = 'Encrypted Texts';
+  // Hide image and informational text from non-added text
+  document.getElementById('div-section').style.display = "none";
   // Get the text of textarea
   const userInput = document.getElementById('user-text').value;
   // Create a new paragraph
