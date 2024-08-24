cd7419386c1679cadce411e82d975b1b9cfe5728
Jefferson Santana <jeffersonsantana1914@outlook.com>
2024-08-23 23:26:42 -0300

    css finish

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+function showNoAlertText() {
+  // Hide image and informational text from non-added text
+  document.getElementById('no-content-alert').style.display = "none";
+}
+showNoAlertText();
+
 function createDecodedText() {
   const heading = document.querySelector('h1');
   heading.innerHTML = 'Encrypted Texts';
-  // Hide image and informational text from non-added text
-  document.getElementById('div-section').style.display = "none";
   // Get the text of textarea
   const userInput = document.getElementById('user-text').value;
   // Create a new paragraph
